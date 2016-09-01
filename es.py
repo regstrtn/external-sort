a90d685fceb0e960138b84213eea47dace21a0bc
regstrtn <regstrtn@gmail.com>
2016-09-02 01:33:28 +0530

    Almost sorted

--- es.py
+++ es.py
@@ -1,41 +1,50 @@
 from __future__ import print_function
 import os
 
-QSIZE = 25
+QSIZE = 3 
 def main():
-				sortlargefile("words1.txt");
+				sortlargefile("w1.txt");
 
 def sortlargefile(filename):
 				fp = open(filename, "r+")
 				offsets = getlineoffsets(fp)
 				numlines = getlines(fp)
-				sortrecursively(fp, offsets, 0, 0, numlines-1, numlines-1)
+				sortrecursively(fp, offsets, 0, 0, numlines, numlines)
 				fp.close()
 
 def writesorted(fp, offsets, depq, start, pos1, pos2, end):
+				if(pos1 == pos2): return fp
+				print("Writesort called with parameters ",start, pos1, pos2, end)
 				fw = open("newfile", "w")
-				fp.seek(offsets[start])
+				fp.seek(0)
+				offsets = getlineoffsets(fp)
 				linesread = 0
 				while (linesread<pos1):
 								linestr = fp.readline()
 								fw.write(linestr)
 								linesread+=1
 				for linestr in depq:
+								print("Depq: "+linestr)
 								fw.write(linestr)
-				fp.seek(offsets[pos2])
-				for linestr in fp:
-								fw.write(linestr)
+				if(pos2<end):
+								print("Pos2: ", pos2)
+								fp.seek(offsets[pos2])
+								for linestr in fp:
+												fw.write(linestr)
 				fw.close()
 				os.rename("newfile", "oldfile")
 				fp = open("oldfile", "r")
 				return fp
 				#write from start to pos1, then depq, then pos2 to end
 
-def writetemp(fp, left, right, depq, offsets, start, pos1, pos2, end):
-				for line in right:
-								print(line)
-				fw = open("newfile", "w")
-				fp.seek(offsets[start])
+def writetemp(fp, depq, offsets, start, pos1, pos2, end):
+				print("Writetemp called with parameters ",start, pos1, pos2, end)
+				if(pos1 == pos2): return fp
+				fw = open("newfile", "w+")
+				left = open("left", "r")
+				right = open("right", "r")
+				fp.seek(0)
+				offsets = getlineoffsets(fp)
 				linesread = 0
 				while (linesread<pos1):
 								linestr = fp.readline()
@@ -47,22 +56,28 @@ def writetemp(fp, left, right, depq, offsets, start, pos1, pos2, end):
 								fw.write(linestr)
 				for linestr in right:
 								fw.write(linestr)
-				fp.seek(offsets[pos2])
-				for linestr in fp:
-								fw.write(linestr)
+				if(pos2<end):
+								print("Pos2: ", pos2)
+								fp.seek(offsets[pos2])
+								for linestr in fp:
+												fw.write(linestr)
+
 				fw.close()
 				os.rename("newfile", "oldfile")
 				fp = open("oldfile", "r")
 				return fp
 
-
-
-
-def sortrecursively(fp,offsets,start, pos1, pos2, end):
+def sortrecursively(fp, offsets, start, pos1, pos2, end):
+				print("Sort called with parameters ",start, pos1, pos2, end)
+				offsets = getlineoffsets(fp)
+				if(pos1==pos2):
+								return
 				depq = []
 				linesread = 0
 				fp.seek(offsets[pos1])
-				if(pos2-pos1<=QSIZE):
+				print("After pos1: "+fp.readline())
+				fp.seek(offsets[pos1])
+				if(pos2-pos1<QSIZE):
 								while (linesread < (pos2-pos1)):
 												linestr = fp.readline()
 												depq.append(linestr)
@@ -72,41 +87,39 @@ def sortrecursively(fp,offsets,start, pos1, pos2, end):
 								fp = writesorted(fp, offsets, depq, start, pos1, pos2, end)
 								pass
 				else:
-								print("is greater than QSIZE")
 								while (linesread<QSIZE):
 												linestr = fp.readline()
 												depq.append(linestr)
 												linesread += 1
 								depq.sort()
-								left = open("left", "r+")
-								right = open("right", "r+")
+								left = open("left", "w+")
+								right = open("right", "w+")
 								leftlines = 0
 								rightlines = 0
 								while (linesread<(pos2-pos1)):
-												print("hello word")
-												right.write("hello world")
 												linestr = fp.readline()
 												linesread +=1
-												if linestr<depq[0]:
+												if (linestr<depq[0]):
 																left.write(linestr)
 																leftlines+=1
-												if linestr>depq[-1]:
+												elif linestr>depq[-1]:
 																right.write(linestr)
 																rightlines+=1
 												else:
+																#print("utely: "+linestr, end = "")
 																right.write(depq[-1])
 																depq[-1] = linestr
 																depq.sort()
-																#print(depq[-1], end = "")
 																rightlines+=1
-								fp = writetemp(fp, left, right, depq, offsets, start, pos1, pos2, end)
-								#sortrecursively(fp, offsets, start, pos1, pos1+leftlines, end)
-								#sortrecursively(fp, offsets, start, pos1+leftlines+QSIZE, pos2, end)
+								left.close()
+								right.close()
+								fp = writetemp(fp, depq, offsets, start, pos1, pos2, end)
+								sortrecursively(fp, offsets, start, pos1, pos1+leftlines, end)
+								sortrecursively(fp, offsets, start, pos1+leftlines+QSIZE, pos2, end)
 								#store. make left. make right.
 								#call sort on left. call sort on right.
-				fp.seek(offsets[pos2])
-				fp.close()
-
+				#fp.seek(offsets[pos2])
+				#fp.close()
 
 def getlines(fp):
 				fp.seek(0);
@@ -122,6 +135,7 @@ def getlineoffsets(fp):
 				for line in fp:
 								lineoffset.append(offset)
 								offset += len(line)
+				lineoffset.append(offset)
 				fp.seek(0)
 				return lineoffset
 
